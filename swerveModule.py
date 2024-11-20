0fc5d16523a28f33a6621cc2baab39dd1389902c
entityJY <jomathya07@gmail.com>
2024-11-20 00:12:30 -0800

    Implemented sysID routines

--- swerveModule.py
+++ swerveModule.py
@@ -8,6 +8,12 @@ from wpimath.system.plant import LinearSystemId
 from wpimath.estimator import KalmanFilter_1_1_1
 from wpimath.system import LinearSystemLoop_1_1_1
 
+from commands2.sysid import SysIdRoutine
+from wpilib.sysid import SysIdRoutineLog
+from wpimath.units import volts
+from wpilib import RobotController
+from commands2 import Command
+
 class SwerveModule:
     
     def __init__(self, drivingCANId: int, turningCANId: int, encoderNum: int, reversedDrive: bool, reversedSteer: bool) -> None:
@@ -43,6 +49,27 @@ class SwerveModule:
         # )
         
         # self.driveReversal = reversedDrive
+        
+        def voltageDrive(voltage: volts):
+            self.drivingSparkMax.setVoltage(volts)
+            
+        self.sys_id_routine = SysIdRoutine(
+            SysIdRoutine.Config(),
+            SysIdRoutine.Mechanism(voltageDrive, self.log, self),
+        )
+        
+    def sysIdQuasistatic(self, direction: SysIdRoutine.Direction) -> Command:
+        return self.sys_id_routine.quasistatic(direction)
+
+    def sysIdDynamic(self, direction: SysIdRoutine.Direction) -> Command:
+        return self.sys_id_routine.dynamic(direction)
+            
+    def log(self, sys_id_routine: SysIdRoutineLog) -> None:
+        sys_id_routine.motor("drive-motor").voltage(
+            self.drivingSparkMax.get() * RobotController.getBatteryVoltage()
+        ).position(self.getPosition().distance).velocity(
+            self.getState().speed
+        )
         
     def getCurrentRotation(self) -> Rotation2d:
         return Rotation2d.fromRotations(self.turningEncoder.get_absolute_position().value_as_double)
